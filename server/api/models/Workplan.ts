d5082b7d7a3b6d7435ca5d861d933f1b83dd5d5b
XoulEater <ncqueescribir@gmail.com>
2024-04-27 10:59:54 -0600

    Refactor Workplan class to remove redundant default values

--- server/api/models/Workplan.ts
+++ server/api/models/Workplan.ts
@@ -2,11 +2,11 @@ import Activity from "./Activity";
 import WorkplanDTO from "../dtos/workplan";
 
 export default class Workplan {
-  private name: string = "";
-  private description: string = "";
-  private activities: Activity[] = [];
-  private year: number = 0;
-  private semester: number = 0;
+  private name: string;
+  private description: string;
+  private activities: Activity[];
+  private year: number;
+  private semester: number;
 
   // Constructor
   constructor(
